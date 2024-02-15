72c9bdb9d4cdbd2b42ff416935b8c595cb08ce16
agrawal542 <gautamagrawal542@gmail.com>
2024-02-16 04:20:12 +0530

    changer server again

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server :{
     proxy :{
-      '/api' :"https://real-time-chat-app-t6sb.onrender.com/"
+      '/api' :"https://real-time-chat-app-t6sb.onrender.com"
     },
   },
   plugins: [react()],
