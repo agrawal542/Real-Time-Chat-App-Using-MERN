aeff3d13448e752ae85e74a9bace34b9bba6be1e
agrawal542 <gautamagrawal542@gmail.com>
2024-02-16 04:06:57 +0530

    change the server fro localhost to serverhost

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server :{
     proxy :{
-      '/api' :"http://localhost:5000"
+      '/api' :"https://real-time-chat-app-t6sb.onrender.com/"
     },
   },
   plugins: [react()],
