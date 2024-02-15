a8bd4729b18531946bbe23dcc67fe99d8b20ed04
agrawal542 <gautamagrawal542@gmail.com>
2024-02-16 04:24:36 +0530

    commit single-chat

--- frontend/src/components/SingleChat.jsx
+++ frontend/src/components/SingleChat.jsx
@@ -12,7 +12,7 @@ import io from "socket.io-client"
 import  Lottie from 'react-lottie'
 import animationData from '../animations/typing.json'
 
-const ENDPOINT = "http://localhost:5000" ;
+const ENDPOINT = "https://real-time-chat-app-t6sb.onrender.com" ;
 const socket = io(ENDPOINT);
 var selectedChatCompare ;
 
